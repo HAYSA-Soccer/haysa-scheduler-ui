597237dcc49be3b7c7eb455fbc9c54cf4dfccae7
dbm1982 <dbm1982@Hotmail.com>
2026-03-24 13:05:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", async function () {
   const calendarEl = document.getElementById("calendar");
 
   // 1. Fetch events from Apps Script
-  const response = await fetch("https://script.google.com/macros/s/AKfycbyHJZ_HOZZFYe8ASTrEKN9axfpXqR0Uu09PG6jgBCXLJCE3jwzYVRqGPSrl3AjwGXoJ/exec");
+  const response = await fetch("https://script.google.com/macros/s/AKfycbwvP6cJtzbPL2-k8GjdyipWj7JWd_PMYpfBL1ItWvYRzehH3GlifErjbyqGQZwz55YRpA/exec");
   const data = await response.json();
 
   // 2. Initialize the calendar with real events
