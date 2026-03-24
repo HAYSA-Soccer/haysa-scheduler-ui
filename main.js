cd689661fbdd2f99c0dd1fac7c8c8ea91f57a5d3
dbm1982 <dbm1982@Hotmail.com>
2026-03-24 12:51:02 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const calendarEl = document.getElementById("calendar");
+
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+    height: "auto",
+    events: []
+  });
+
+  calendar.render();
+});
